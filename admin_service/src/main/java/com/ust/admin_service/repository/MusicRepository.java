36a75c0242c4a064870cead144b3a11a06d2fbc6
Yashwanth Ravula <94450368+Yashwanth-Ravula1025@users.noreply.github.com>
2023-06-13 17:09:31 +0530

    Delete admin_service directory

--- admin_service/src/main/java/com/ust/admin_service/repository/MusicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ust.admin_service.repository;
-
-import com.ust.admin_service.entity.Music;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MusicRepository extends JpaRepository<Music,Long> {
-}
